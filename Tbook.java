166b43453994f25305e998ebf3f54f8c82d4d719
Vlad_Spirin <vlad.spirin1@yandex.ru>
2024-01-26 17:01:50 +0400

    End

--- Tbook.java
+++ Tbook.java
@@ -12,5 +12,17 @@ public class Tbook {
     public void add(String name, String phoneNumber) {
         TBook.computeIfAbsent(name, k -> new ArrayList<>()).add(phoneNumber);
     }
-
+    public List<String> get(String name) {
+        return TBook.getOrDefault(name, new ArrayList<>());
+    }
+    public static void main(String[] args) {
+        Tbook phoneBook = new Tbook();
+        Tbook.add("Vladislav", "+79022399878");
+        Tbook.add("Maxim", "+79399764976");
+        Tbook.add("Vladislav", "+79026758732");
+        Tbook.add("Genadiy", "+79278563856");
+        System.out.println("Телефоны Vladislav: " + Tbook.get("Vladislav"));
+        System.out.println("Телефоны Maxim: " + Tbook.get("Maxim"));
+        System.out.println("Телефоны Genadiy: " + Tbook.get("Genadiy"));
+    }
 }
