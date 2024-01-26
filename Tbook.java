6620d09f1824b92f492fc2dbd05fd5225f2b92df
Vlad_Spirin <vlad.spirin1@yandex.ru>
2024-01-26 16:56:22 +0400

    Add numPhone

--- Tbook.java
+++ Tbook.java
@@ -1,2 +1,16 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Tbook {
+    private final Map<String, List<String>> TBook;
+
+    public Tbook(Map<String, List<String>> phoneBook) {
+        this.TBook = new HashMap<>();
+    }
+    public void add(String name, String phoneNumber) {
+        TBook.computeIfAbsent(name, k -> new ArrayList<>()).add(phoneNumber);
+    }
+
 }
